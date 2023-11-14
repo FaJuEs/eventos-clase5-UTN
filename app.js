f481d177fe5731f0ad579ae3af3ed3b7db079384
Fabiana <139273452+FaJuEs@users.noreply.github.com>
2023-11-14 20:21:23 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var boton = document.getElementById("boton");
+var parrafo = document.getElementById("parrafo");
+
+
+boton.onclick = function () {
+parrafo.style.background = "red";
+parrafo.style.fontSize = "45px";
+
+}
